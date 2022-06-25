5e13accd6d81b7258226d8396d662ec4965eba58
Andraz Polajzer <andraz.polajzer@rettro.si>
2022-06-25 21:13:58 +0200

    added search input field

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,19 @@
 
 <div class="flex justify-center items-center h-full">
     <div class="w-1/2">
+        <div class="mb-12 relative">
+            <input
+                    id="search"
+                    class="w-full px-3 py-2 rounded-xl drop-shadow-lg"
+                    placeholder="Search anything"
+            />
+            <label
+                    for="search"
+                    class="absolute right-3 top-0 h-full flex items-center"
+            >
+                <i class="ph-magnifying-glass"></i>
+            </label>
+        </div>
         <FrequentVisits/>
     </div>
 </div>
